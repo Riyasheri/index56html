fd65c96d48458471eeb667ab8db9329b8d27dffd
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-06 19:55:21 +0530

    searchbook

---
+++ src/app/searchbook-app/searchbook-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbook-app',
+  templateUrl: './searchbook-app.component.html',
+  styleUrls: ['./searchbook-app.component.css']
+})
+export class SearchbookAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
